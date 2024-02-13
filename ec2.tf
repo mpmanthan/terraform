6dafa3176a71a4abe6e029c39d205dd92e8b564f
manthan <mpmanthan07@gmail.com>
2024-02-13 11:12:40 +0000

    ec2 with package

--- ec2.tf
+++ ec2.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "webcode" {
 		sudo apt-get install -y apache2
 		sudo systemctl start apache2
 		sudo systemctl enable apache2
-		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+		
 	EOF"
  
 
