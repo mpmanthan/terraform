19fe998c8242feaedc4795b5f66f930c024cf83b
manthan <mpmanthan07@gmail.com>
2024-02-13 11:16:48 +0000

    ec2 with package and sh file

--- ec2.tf
+++ ec2.tf
@@ -9,14 +9,6 @@ resource "aws_instance" "webcode" {
   ami           = "ami-03f4878755434977f"
   instance_type = "t2.micro"
   key_name      = "full"
-  user_data = " 
-		#! /bin/bash
-                sudo apt-get update
-		sudo apt-get install -y apache2
-		sudo systemctl start apache2
-		sudo systemctl enable apache2
-		
-	"
  
 
   tags = {
