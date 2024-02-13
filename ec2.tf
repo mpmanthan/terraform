5ef350ce0959a5420ef96035af79831341cb8079
manthan <mpmanthan07@gmail.com>
2024-02-13 11:18:39 +0000

    ec2 with package

--- ec2.tf
+++ ec2.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "webcode" {
   ami           = "ami-03f4878755434977f"
   instance_type = "t2.micro"
   key_name      = "full"
- 
+  user_data = "${file("install_apache.sh")}"
 
   tags = {
     name = "great"
