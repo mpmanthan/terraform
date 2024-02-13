f3f947f47e85cc5d58021a16d0d808453be653ec
manthan <mpmanthan07@gmail.com>
2024-02-13 09:46:01 +0000

    2 add

--- ec2.tf
+++ ec2.tf
@@ -2,10 +2,8 @@ provider "aws" {
   
   }
 
-
-
 resource "aws_instance" "webcode" {
-  ami           = "ami-0dcc1e21636832c5d"
+  ami           = "ami-03f4878755434977f"
   instance_type = "t2.micro"
   key_name      = "full"
   tags = {
