5b882bcfa3857d34ae450e82f79ab1603812242c
manthan <mpmanthan07@gmail.com>
2024-02-13 11:08:25 +0000

    ec2 with package

--- ec2.tf
+++ ec2.tf
@@ -9,14 +9,14 @@ resource "aws_instance" "webcode" {
   ami           = "ami-03f4878755434977f"
   instance_type = "t2.micro"
   key_name      = "full"
-   user_data = user_data = << EOF
+  user_data = << EOF
 		#! /bin/bash
                 sudo apt-get update
 		sudo apt-get install -y apache2
 		sudo systemctl start apache2
 		sudo systemctl enable apache2
 		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
-	EOF>>
+	EOF
  
 
   tags = {
