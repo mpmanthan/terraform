4c9b08ac1b453fdb4805bc4ed4c2b9adcbc64e31
manthan <mpmanthan07@gmail.com>
2024-02-13 11:02:27 +0000

    ec2 with package

--- ec2.tf
+++ ec2.tf
@@ -1,25 +1,26 @@
-#
-#provider "aws" {
-#  region     = "ap-south-1"
-#  access_key = "AKIATIP3WBBILLRY4ZLP"
-#  secret_key = "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"
-#}
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIATIP3WBBILLRY4ZLP"
+  secret_key = "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"
+}
 
 resource "aws_instance" "webcode" {
   ami           = "ami-03f4878755434977f"
   instance_type = "t2.micro"
   key_name      = "full"
-   user_data = 
-!/bin/bash
- apt-get update
- apt-get install -y apache2
- apt-get install -y docker.io
- systemctl start apache2
- systemctl enable apache2
+   user_data = user_data = << EOF
+		#! /bin/bash
+                sudo apt-get update
+		sudo apt-get install -y apache2
+		sudo systemctl start apache2
+		sudo systemctl enable apache2
+		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+	EOF
  
 
   tags = {
-    name = "try"
+    name = "great"
   }
 
 }
