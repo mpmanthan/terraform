36267e457f35e9293e5e02f29cbc87bda99179ec
manthan <mpmanthan07@gmail.com>
2024-02-13 11:06:15 +0000

    ec2 with package

--- ec2.tf
+++ ec2.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "webcode" {
 		sudo systemctl start apache2
 		sudo systemctl enable apache2
 		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
-	EOF
+	EOF>>
  
 
   tags = {
