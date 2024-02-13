020376090bb2143e70fe1580ab8c6411be6c815b
manthan <mpmanthan07@gmail.com>
2024-02-13 11:13:51 +0000

    ec2 with package

--- ec2.tf
+++ ec2.tf
@@ -9,14 +9,14 @@ resource "aws_instance" "webcode" {
   ami           = "ami-03f4878755434977f"
   instance_type = "t2.micro"
   key_name      = "full"
-  user_data = " << EOF
+  user_data = " 
 		#! /bin/bash
                 sudo apt-get update
 		sudo apt-get install -y apache2
 		sudo systemctl start apache2
 		sudo systemctl enable apache2
 		
-	EOF"
+	"
  
 
   tags = {
