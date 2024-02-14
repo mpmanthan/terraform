53fc6609211079a4413bf9af82099162cce1f367
manthan <mpmanthan07@gmail.com>
2024-02-14 07:13:26 +0000

    web page

--- install_apache.sh
+++ install_apache.sh
@@ -3,4 +3,19 @@ sudo apt-get update
 sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
-echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo apt-get install docker.io  docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin 
+
+
+sudo systemctl start docker.io
+sudo systemctl enable docker.io
+
+sudo docker build -t solo:v1 .
+sudo docker run -d -p 80:8080 -name xyz solo:v1
+
